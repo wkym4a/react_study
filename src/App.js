9b7f8519b04dfdded6ae8484566f532fc1a9e4c9
若山侑輝 <wkym_4a@icloud.com>
2020-04-28 23:54:29 +0900

    152p_3-21まで

--- src/App.js
+++ src/App.js
@@ -5,14 +5,47 @@ import './App.css';
 
 
 class App extends Component {
+
+
+  msgStyle = {
+    fontSize:"24pt",
+    color:"#900",
+    margin:"20px 0px",
+    padding: "5px",
+    borderBottom:"2px solid #900"
+  }
+  btnStyle = {
+    fontSize:"20pt",
+    padding:"0px 10px"
+  }
+
+
+  constructor(props){
+    super(props);
+    this.state = {
+      counter:0,
+      msg:'count start!',
+    };
+    this.doAction = this.doAction.bind(this);
+  }
+
+
+  doAction(e){
+    this.setState((state)=> ({
+      counter: state.counter + 1,
+      msg: 'count: ' + state.counter
+    }));
+  }
+
   render(){
     return <div>
       <h1>React</h1>
-      <Rect x="50" y="50" w="150" h="150" c="#6ff9" r="50" />
-      <Rect x="150" y="100" w="150" h="150" c="#f6f9" r="75" />
-      <Rect x="100" y="150" w="150" h="150" c="#6669" r="25" />
+      <p style={this.msgStyle}>{this.state.msg}</p>
+      <button style={this.btnStyle} onClick={this.doAction}>Click</button>
     </div>;
   }
+
+
 }
 
 
