4023d83de223d834853cd7e3eb1b08d292891ddb
若山侑輝 <wkym_4a@icloud.com>
2020-05-01 15:42:28 +0900

    209p、4-1まで

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-// import {App2} from './App';
-import * as serviceWorker from './serviceWorker';
 
+
+// ステートの値
+let state_value = {
+    counter:0,
+    message:"COUNTER"
+}
+
+
+// レデューサー
+function counter( state = state_value, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+        return {
+            counter:state.counter + 1,
+            message:"INCREMENT"
+        };
+        case 'DECREMENT':
+        return {
+            counter:state.counter - 1,
+            message:"DECREMENT"
+        };
+        default:
+        return state;
+    }
+}
+
+
+// ストアを作成
+let store = createStore(counter);
+
+
+// 表示をレンダリング
 ReactDOM.render(
-  <App/>,document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
